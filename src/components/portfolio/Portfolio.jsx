63ddbdc827c260e1f56dbb3dee810531cd7e95bc
shemil01 <shemilmk1033@gmail.com>
2024-10-08 09:51:16 +0530

    updated

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,50 +1,108 @@
 import { useRef } from "react";
 import "./portfolio.scss";
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
+
+// animation
+const itemVariants = {
+  initial: {
+    x: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.8,
+      ease: "easeOut",
+      staggerChildren: 0.2,
+    },
+  },
+};
+const variants = {
+  initial: {
+    x: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.8,
+      ease: "easeOut",
+      staggerChildren: 0.2,
+    },
+  },
+};
 
 const items = [
   {
     id: 1,
-    title: "Fullstack e-commerce website",
-    img: "/plashoe.png", 
-    source:"https://react-e-commerce-project-eight.vercel.app/",
-    desc: "Developed a full-fledged e-commerce platform using the MERN stack, improving UI to reduce bounce rates by 50% and increase conversions by 20%. Implemented secure user authentication with JWT and built a scalable backend with Node.js, Express.js, and MongoDB.",
+    title: "Spotify clone",
+    img: "/spotify.png",
+    source: "https://github.com/shemil01/spotify-client",
+    link: " https://spotify-client-psi-black.vercel.app",
+    desc: "Developed a Spotify clone from scratch using the MERN stack and Tailwind CSS, replicating core features like JWT-based authentication, file uploads with Multer, and cloud storage via Cloudinary. Created playlist management, advanced search with letter suggestions, and built an admin dashboard for user and content management.",
   },
   {
     id: 2,
-    title: "Spotify clone",
-    img: "/spotify.png",  
-    source:" https://spotify-client-psi-black.vercel.app",
-    desc:"Developed a Spotify clone from scratch using the MERN stack and Tailwind CSS, replicating core features like JWT-based authentication, file uploads with Multer, and cloud storage via Cloudinary. Created playlist management, advanced search with letter suggestions, and built an admin dashboard for user and content management."
+    title: " e-commerce website",
+    img: "/plashoe.png",
+    source: "https://github.com/shemil01/plashoe-frontend",
+    link: "https://react-e-commerce-project-eight.vercel.app/",
+    desc: "Developed a full-fledged e-commerce platform using the MERN stack, improving UI to reduce bounce rates by 50% and increase conversions by 20%. Implemented secure user authentication with JWT and built a scalable backend with Node.js, Express.js, and MongoDB.",
   },
+
   {
     id: 3,
+    title: " The Daily Biz",
+    img: "https://www.searchenginejournal.com/wp-content/uploads/2020/08/7-ways-a-blog-can-help-your-business-right-now-5f3c06b9eb24e.png",
+    source: "https://github.com/shemil01/blog-app-nextjs",
+    link: "https://the-daily-biz.vercel.app/blog",
+    desc: "A blog app built using Next.js and TypeScript, featuring both dark mode and light mode for a personalized user experience. The app includes a convenient copy button for easily copying content. It’s designed to be fast, responsive, and user-friendly, with seamless navigation across posts.",
+  },
+  {
+    id: 4,
     title: " E-commerce backend",
-    img: "/backend.png", 
-    source:"https://github.com/shemil01/backend-project",
-  desc:"Developed a robust e-commerce backend using Node.js, Express, and MongoDB. Designed and implemented RESTful APIs for managing products, user authentication, order processing, and payment integration. Ensured secure transactions with JWT-based authentication and encryption."
+    img: "/backend.png",
+    link: "https://github.com/shemil01/backend-project",
+    source: "https://github.com/shemil01/backend-project",
+    desc: "Developed a robust e-commerce backend using Node.js, Express, and MongoDB. Designed and implemented RESTful APIs for managing products, user authentication, order processing, and payment integration. Ensured secure transactions with JWT-based authentication and encryption.",
   },
 ];
 
 const Single = ({ item }) => {
   const ref = useRef();
-  const { scrollYProgress } = useScroll({
-    target: ref,
-  });
-
-  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
 
   return (
     <section>
-      <div className="container" >
+      <div className="container">
         <div className="wrapper">
-          <div className="imageContainer" ref={ref}>
+          <motion.div
+            variants={variants}
+            initial="initial"
+            whileInView="animate"
+            className="imageContainer"
+            ref={ref}
+          >
             <img src={item.img} alt={item.title} />
-          </div>
-          <motion.div className="textContainer" style={{ y }}>
+            <div className="overlay">
+              <a href={item.link} target="_blank" rel="noopener noreferrer">
+                Visit Website
+              </a>
+            </div>
+          </motion.div>
+          <motion.div
+            ref={ref}
+            variants={itemVariants}
+            initial="initial"
+            whileInView="animate"
+            className="textContainer"
+          >
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-           <a href={item.source}> <button>See demo</button></a>
+            <a href={item.source}>
+              <button>Source code</button>
+            </a>
           </motion.div>
         </div>
       </div>
