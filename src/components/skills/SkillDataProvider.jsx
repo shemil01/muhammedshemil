540099c91e065c7b80367cb93488bf9410fe247f
shemil01 <shemilmk1033@gmail.com>
2024-10-09 09:47:37 +0530

    updated

--- src/components/skills/SkillDataProvider.jsx
+++ src/components/skills/SkillDataProvider.jsx
@@ -1,15 +1,16 @@
+// SkillDataProvider.js
 import React from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
-const SkillDataProvider = ({ src, width, height, index }) => {
+const SkillDataProvider = ({ src, width, height, index, title }) => {
   const { ref, inView } = useInView({
     triggerOnce: true,
   });
 
   const imageVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1 },
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1 },
   };
 
   const animationDelay = 0.3;
@@ -20,16 +21,26 @@ const SkillDataProvider = ({ src, width, height, index }) => {
       initial="hidden"
       variants={imageVariants}
       animate={inView ? 'visible' : 'hidden'}
-      custom={index}
       transition={{ delay: index * animationDelay }}
-      className="flex justify-center" // Center each skill image
+      className="skill-card"
     >
-      <img
-        src={src}
-        width={width}
-        height={height}
-        alt='skill image'
-      />
+      <div className="card-inner">
+        {/* Front of the card */}
+        <div className="card-front">
+          <img
+            src={src}
+            width={width}
+            height={height}
+            alt={title}
+            className="skill-image"
+          />
+        </div>
+        
+        {/* Back of the card */}
+        <div className="card-back">
+          <h3 className="skill-title">{title}</h3>
+        </div>
+      </div>
     </motion.div>
   );
 };
