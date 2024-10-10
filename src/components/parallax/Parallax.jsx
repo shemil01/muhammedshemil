2bddba9909eb67ca9e77355f13c6b439c6afd4ba
shemil01 <shemilmk1033@gmail.com>
2024-10-10 09:47:17 +0530

    updated

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -3,9 +3,6 @@ import "./parallax.scss";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 const Parallax = ({ type }) => {
-  
-
-
   const ref = useRef();
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -13,10 +10,6 @@ const Parallax = ({ type }) => {
   });
   const yText = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
   const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-
-
-
-
 
   return (
     <div
@@ -29,14 +22,10 @@ const Parallax = ({ type }) => {
             : "linear-gradient(180deg, #111132, #505064)",
       }}
     >
-      
-
-      <motion.div className="skills-container">
-      <motion.h1 style={{ y: yText }}>What We DO</motion.h1>
-        <motion.div className="skills">
-          
-        </motion.div>
-      </motion.div>
+      <motion.h1 style={{ y: yText }}>
+        {" "}
+        {type === "services" ? "What We Do?" : "What We Did?"}
+      </motion.h1>
 
       <motion.div className="mountains"></motion.div>
       <motion.div
