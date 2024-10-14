11dd45f34da2176f434743ff2d41e1ce7c4a8721
shemil01 <shemilmk1033@gmail.com>
2024-10-14 09:47:48 +0530

    updated

--- src/components/skills/SkillText.jsx
+++ src/components/skills/SkillText.jsx
@@ -13,7 +13,7 @@ const SkillText = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
- Used Tools Technologies
+ Used Tools And Technologies
           </h1>
         </motion.div>
         <motion.div
