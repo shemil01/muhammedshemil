cb341b84006a09d4597116374489e0118506e387
shemil01 <shemilmk1033@gmail.com>
2025-06-17 16:04:07 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ReactGA from 'react-ga4';
 
 import App from "./App.jsx";
-
+ReactGA.initialize('G-L95JWV5KYY')
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
    
